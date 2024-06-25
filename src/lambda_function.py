1e4fb9cc060f7aab414ce0b42f327b62a17310bb
jonathanalves90 <jonathan.alves90@outlook.com>
2024-06-25 19:19:10 -0300

    First Deploy

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! - V1.0.0 -')
+        'body': json.dumps('Hello from Lambda! - V1.0.0 - First Test')
     }
